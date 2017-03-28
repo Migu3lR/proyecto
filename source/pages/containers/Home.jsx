d0ca83ecc8be11fc781b838f4eb8ac4b5f3f3615
Miguel Romero <ingmromero34@gmail.com>
2017-03-28 18:27:57 -0500

    Scroll infinito

--- source/pages/containers/Home.jsx
+++ source/pages/containers/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Post from '../../posts/containers/Post.jsx';
 import Loading from '../../shared/components/Loading.jsx';
@@ -7,7 +7,7 @@ import Loading from '../../shared/components/Loading.jsx';
 import api from '../../api.jsx';
 
 class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -16,19 +16,57 @@ class Home extends Component {
       loading: true,
 
     };
+
+    this.handleScroll = this.handleScroll.bind(this);
   }
-  async componentDidMount(){
+  async componentDidMount() {
     const posts = await api.posts.getList(this.state.page);
 
     this.setState({
       posts,
-      page: this.state.page +1,
+      page: this.state.page + 1,
       loading: false
     });
 
+    window.addEventListener('scroll', this.handleScroll);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    if (this.state.loading) return null;
+
+    const scrolled = window.scrollY;
+    const viewportHeight = window.innerHeight;
+    const fullHeight = document.body.clientHeight;
+
+    if (!(scrolled + viewportHeight + 300 >= fullHeight)) {
+      return null;
+    }
+
+    this.setState({
+      loading: true,
+    }, async () => {
+      try {
+        const posts = await api.posts.getList(this.state.page)
+        this.setState({
+          posts: this.state.posts.concat(posts),
+          page: this.state.page + 1,
+          loading: false,
+        })
+      } catch (error) {
+        console.error(error);
+        this.setState({
+          loading: false,
+        })
+      }
+    })
+  }
+
   render() {
-    return(
+    return (
       <section name="Home">
         <h1> Home </h1>
 
