2fed3ae46ecf0334a7e38d1d0cf9f4a95ec7b560
Miguel Romero <ingmromero34@gmail.com>
2017-03-30 23:19:31 -0500

    Traducciones

--- source/shared/components/Loading.jsx
+++ source/shared/components/Loading.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { FormattedMessage } from 'react-intl';
 
 function Loading() {
   return (
     <h3>
-      Loadings data...
-      </h3>
+      <FormattedMessage id="loading" />
+    </h3>
   );
 }
 
